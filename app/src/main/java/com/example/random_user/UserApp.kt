5d0a031f2d49788f77fa440f3ebdb57870e751a7
Native Developer <lexusshatov2@gmail.com>
2021-07-20 10:40:39 +0300

    fixes

--- app/src/main/java/com/example/random_user/UserApp.kt
+++ app/src/main/java/com/example/random_user/UserApp.kt
@@ -1,19 +1,12 @@
 package com.example.random_user
 
 import android.app.Application
-import com.example.random_user.model.di.AppComponent
-import com.example.random_user.model.di.DaggerAppComponent
-import com.example.random_user.model.di.repository.RepositoryDecoratorComponent
-import javax.inject.Inject
 
 class UserApp: Application() {
-    @Inject
-    lateinit var appComponent: AppComponent
+
 
     override fun onCreate() {
         super.onCreate()
-        //DI.getInstance().init { applicationContext }
-        DaggerAppComponent.builder()
-            .build().inject(this)
+
     }
 }
--- app/src/main/java/com/example/random_user/model/di/repository/base/data/DataComponent.kt
+++ app/src/main/java/com/example/random_user/model/di/repository/base/data/DataComponent.kt
@@ -1,6 +1,5 @@
 package com.example.random_user.model.di.repository.base.data
 
-import android.content.Context
 import com.example.random_user.model.local.UserDatabase
 import com.example.random_user.model.remote.UserApi
 import dagger.Subcomponent
@@ -8,7 +7,6 @@ import dagger.Subcomponent
 @Subcomponent(modules = [ApiModule::class, LocalModule::class])
 interface DataComponent {
 
-    fun inject(context: Context)
     fun userApi(): UserApi
     fun localDatabase(): UserDatabase
 }
